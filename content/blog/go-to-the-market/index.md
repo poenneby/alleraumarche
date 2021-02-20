9e17c57b34058003ec7dea3b70541d70f09e6330
Peter Onneby <ponneby@xebia.fr>
2021-02-20 21:35:57 +0100

    First publish

---
+++ content/blog/go-to-the-market/index.md
@@ -0,0 +1,7 @@
+---
+title: Go to the market!
+date: "2021-02-20T21:10:03.284Z"
+description: "Go to the market!"
+---
+
+# Go to the market
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Aller au Marché`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Peter Onneby`,
+      summary: `who lives and works in France.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `poenneby`,
     },
   },
   plugins: [
